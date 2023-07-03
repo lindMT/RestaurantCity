414dcbf6aa97479b8c4f3271cf7199826cf65c4a
lindMT <103123352+lindMT@users.noreply.github.com>
2023-07-03 19:09:43 +0800

    week PICKER fix

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,10 +1,7 @@
-// WEEKLY
-function weeklyReport(weekId){
-    var startDate,
+var startDate,
         endDate;
-    var weekVar = document.getElementById('weekId');
-
-      $(weekVar).datepicker({
+        
+      $('#weekpicker').datepicker({
         autoclose: true,
         format :'mm/dd/yyyy',
         forceParse :false
@@ -13,31 +10,30 @@ function weeklyReport(weekId){
         var date = e.date;
         startDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay());
         endDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay()+6);
-        //$(weekVar).datepicker("setDate", startDate);
-        $(weekVar).datepicker('update', startDate);
-        $(weekVar).val((startDate.getMonth() + 1) + '/' + startDate.getDate() + '/' +  startDate.getFullYear() + ' - ' + (endDate.getMonth() + 1) + '/' + endDate.getDate() + '/' +  endDate.getFullYear());
+        //$('#weekpicker').datepicker("setDate", startDate);
+        $('#weekpicker').datepicker('update', startDate);
+        $('#weekpicker').val((startDate.getMonth() + 1) + '/' + startDate.getDate() + '/' +  startDate.getFullYear() + ' - ' + (endDate.getMonth() + 1) + '/' + endDate.getDate() + '/' +  endDate.getFullYear());
     });
         
         
         //new
         $('#prevWeek').click(function(e){
-          var date = $(weekVar).datepicker('getDate');
+          var date = $('#weekpicker').datepicker('getDate');
           //dateFormat = "mm/dd/yy"; //$.datepicker._defaults.dateFormat;
           startDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay()- 7);
           endDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay() - 1);
-          $(weekVar).datepicker("setDate", new Date(startDate));
-          $(weekVar).val((startDate.getMonth() + 1) + '/' + startDate.getDate() + '/' +  startDate.getFullYear() + ' - ' + (endDate.getMonth() + 1) + '/' + endDate.getDate() + '/' +  endDate.getFullYear());
+          $('#weekpicker').datepicker("setDate", new Date(startDate));
+          $('#weekpicker').val((startDate.getMonth() + 1) + '/' + startDate.getDate() + '/' +  startDate.getFullYear() + ' - ' + (endDate.getMonth() + 1) + '/' + endDate.getDate() + '/' +  endDate.getFullYear());
                  
           return false;
         });
         $('#nextWeek').click(function(){
-          var date = $(weekVar).datepicker('getDate');
+          var date = $('#weekpicker').datepicker('getDate');
           //dateFormat = "mm/dd/yy"; // $.datepicker._defaults.dateFormat;
           startDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay()+ 7);
           endDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay() + 13);
-          $(weekVar).datepicker("setDate", new Date(startDate));
-          $(weekVar).val((startDate.getMonth() + 1) + '/' + startDate.getDate() + '/' +  startDate.getFullYear() + ' - ' + (endDate.getMonth() + 1) + '/' + endDate.getDate() + '/' +  endDate.getFullYear());
+          $('#weekpicker').datepicker("setDate", new Date(startDate));
+          $('#weekpicker').val((startDate.getMonth() + 1) + '/' + startDate.getDate() + '/' +  startDate.getFullYear() + ' - ' + (endDate.getMonth() + 1) + '/' + endDate.getDate() + '/' +  endDate.getFullYear());
             
           return false;
         });
-}
