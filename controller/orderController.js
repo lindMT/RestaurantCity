1c35ae444f1e6afae85a3875d0123af0efe86ff2
lindMT <103123352+lindMT@users.noreply.github.com>
2023-07-05 00:09:33 +0800

    unfinished orderprocessing

--- controller/orderController.js
+++ controller/orderController.js
@@ -7,6 +7,20 @@ const orderController = {
     },
 
     processOrder: function(req, res){
+        // Process Order
+        // var quantityArray = req.body.quantity;
+        // var dishIdArray = req.body.dishId;
+        // console.log("QTY ARRAY:");
+        // console.log(quantityArray);
+        // console.log("DISH ID ARRAY:");
+        // console.log(dishIdArray);
+
+        var totalPrice = req.body.totalPrice;
+        console.log(totalPrice);
+        // var one = req.body;
+        // console.log(one)
+        // TODO (Lind):
+        //  - screen to show order success / order unfilfilled (display lacking ingredients/dish)
         res.render('orderTerminal');
     }
 }
