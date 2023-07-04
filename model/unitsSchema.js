2d3648c367d0d2d0efbad08c510400a5bebdb974
gabbycoloma <colomalouis@gmail.com>
2023-07-04 21:34:35 +0800

    Ingredients, IngreVariations and units Schemas

---
+++ model/unitsSchema.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const unitsSchema = new Schema({
+    unitName: {
+        type: String,
+        required: true,
+    },
+    unitSymbol: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    category: {
+        type: String,
+        enum: ["wet", "dry"],
+        required: true,
+    },
+});
+
+module.exports = mongoose.model('units', unitsSchema);
