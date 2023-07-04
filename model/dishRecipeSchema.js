960b512c600bd6dacb89c9c79f8c30a3347b9dfd
djdcaballero <don_caballero@dlsu.edu.ph>
2023-07-04 21:39:47 +0800

    Added schemas with name "dish"
    dishes, dishCategory, dishRecipe

---
+++ model/dishRecipeSchema.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const dishRecipeSchema = new Schema ({
+    dishID: [
+        {
+            type: Schema.Types.ObjectId, ref: 'dishes',
+            required: true
+        }
+    ],
+
+    ingreID: [
+        {
+            type: Schema.Types.ObjectId, ref: 'ingredients',
+            required: true
+        }
+    ],
+
+    metricWeight: {
+        type: mongoose.Types.Decimal128,
+        required: true
+    },
+
+    metricUnitID: [
+        {
+            type: Schema.Types.ObjectId, ref: 'metricUnit',
+            required: true
+        }
+    ],
+
+    chefWeight: {
+        type: mongoose.Types.Decimal128,
+        required: trye
+    },
+
+    chefUnitID: [
+        {
+            type: Schema.Types.ObjectId, ref: 'chefUnit',
+            required: true
+        }
+    ],
+
+});
+
+module.exports = mongoose.model('dishRecipe', dishRecipeSchema);
