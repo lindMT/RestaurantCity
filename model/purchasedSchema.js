cf55b842085148f9c9918611409402cc1a0aedf0
Jm Mojica <jmmojica0701@gmail.com>
2023-07-04 21:09:05 +0800

    Schemas

---
+++ model/purchasedSchema.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const purchasedSchema = new Schema( {
+    ingreID: [
+        {
+            type: Schema.Types.ObjectId, ref: 'ingredients',
+            required: true,
+        }
+      ],
+
+    date: { 
+        type: String, 
+        required: true,
+    },
+
+    varID: [
+        {
+            type: Schema.Types.ObjectId, ref: 'ingreVariations',
+            required: true,
+        }
+      ],
+
+    qty: {
+        type: Int,
+        required: true
+    },
+
+    doneBy: [
+        {
+            type: Schema.Types.ObjectId, ref: 'users',
+            required: true,
+        }
+      ],
+
+});
+
+module.exports = mongoose.model('purchased', purchasedSchema);
