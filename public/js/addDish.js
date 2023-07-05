080c6e51c0923e69d836af182891e51f2801dbe0
JamesLorencee <james.seril.12@gmail.com>
2023-07-05 19:30:57 +0800

    Modified addDish.ejs, addDish.js
    Modified
    addDish.ejs
    - Fixed the Div classes
    - Added Add ingredients form
    Missing:
    - No Delete function yet for forms
    
    addDish.js
    - Created function duplicateForm()
    Missing:
    No Delete Form Yet
    No Exception yet if there is 1 ingredient form remaining

--- public/js/addDish.js
+++ public/js/addDish.js
@@ -1,3 +1,28 @@
 function addInputFields(){
     
 }
+
+const parentDiv = document.getElementById('form-row');
+
+function duplicateForm() {
+  let formRow = document.createElement('div');
+  formRow.className = 'form-row';
+
+  let formRowContent = document.getElementById('form-row-content').cloneNode(true);
+
+  // Remove all label elements
+  const labels = formRowContent.querySelectorAll('label');
+  labels.forEach((label) => {
+    label.remove();
+  });
+
+  formRow.appendChild(formRowContent);
+
+  parentDiv.appendChild(formRow);
+
+  // To have a margin when adding new Ingredient form
+  formRow.style.marginTop = '10px';
+
+}
+
+
