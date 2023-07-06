f3c5def370d2b27226023e5cbbbc3de57bff3b63
djdcaballero <don_caballero@dlsu.edu.ph>
2023-07-07 00:10:50 +0800

    Added TODO list for addDish

--- controller/addDishController.js
+++ controller/addDishController.js
@@ -3,6 +3,7 @@ const User = require('../model/usersSchema.js');
 const Dish = require('../model/dishSchema.js');
 const DishCategory = require('../model/dishCategorySchema.js');
 const DishRecipe = require('../model/dishRecipeSchema.js');
+const Ingredients = require('../model/ingredientsSchema.js');
 const bcrypt = require("bcrypt");
 
 const addDishController = {
@@ -12,7 +13,18 @@ const addDishController = {
     },
 
     postAddDish: async(req, res) => {
-        // Fetch category in (inputCategory???), where data is from category table
+        //TODO
+        // 1) Counter check if dish name is existing in table
+            // If exists, *create pop up if user wants to change dish(???)*
+                // If yes, set old dish's "isActive" to false
+                // If no, close pop up
+        // 2) If submit is clicked
+            // Validate if all data are appropriate
+                // If appropriate, store RECIPE in array of ingredients (recipe of dish) and DISH in dish table
+                // If inappropriate, error handling
+        // 3) Finalize categories
+
+        // Fetch category in views, where data is from category table
         const category = await DishCategory.findOne({ category: req.body.inputCategory });
         const currentDate = Date();
 
