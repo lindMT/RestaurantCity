f968472d5562dceeffb2af8c6d7d48afb6a3c61c
JamesLorencee <james.seril.12@gmail.com>
2023-07-06 18:11:50 +0800

    Modified manageDishes.ejs, manageDishes.js
    Modified manageDishes
    - Added a error checking that the remove dish button is 'Disabled' if there are no checkboxes selected.
    It will only enable if there are checkboxes selected
    
    Note: Please check if the code is concise and clear that it could work on the backend

--- public/js/manageDishes.js
+++ public/js/manageDishes.js
@@ -1,6 +1,32 @@
-function deleteRow(){
-    document.querySelectorAll('#dishTable .select:checked').forEach(e => {
-      e.closest('tr').remove();
-    });
-  }
+const removeDishBtn = document.getElementById('removeDishBtn');
 
+function deleteRow() {
+  const selectedCheckboxes = document.querySelectorAll('#dishTable .select:checked');
+  selectedCheckboxes.forEach(checkbox => {
+    const row = checkbox.closest('tr');
+    row.remove();
+  });
+
+  // Check if any checkboxes are selected
+  const checkboxes = document.querySelectorAll('#dishTable .select');
+  const disableButton = selectedCheckboxes.length === 0;
+
+  // Disable or enable the button based on the selection
+  removeDishBtn.disabled = disableButton;
+}
+
+// Checks if the checkbox is selected, if there are no checkboxes, the Remove Button is DISABLED
+function handleCheckboxClick() {
+  const checkedCheckboxes = document.querySelectorAll('#dishTable .select:checked');
+  const disableButton = checkedCheckboxes.length === 0;
+  removeDishBtn.disabled = disableButton;
+
+  // Changes the color if the button is disabled or enabled
+  removeDishBtn.classList.toggle('btn-secondary', disableButton);
+  removeDishBtn.classList.toggle('btn-danger', !disableButton);
+}
+
+// Listen for checkbox change events
+document.querySelectorAll('#dishTable .select').forEach(checkbox => {
+  checkbox.addEventListener('change', handleCheckboxClick);
+});
