31143da9a5f65275d5eddcd99aa9755ef27ab53b
gabbycoloma <colomalouis@gmail.com>
2023-07-07 17:01:33 +0800

    update total net weight of inventory when adding variation (initial)

--- controller/recordAddtlController.js
+++ controller/recordAddtlController.js
@@ -7,18 +7,18 @@ const bcrypt = require("bcrypt");
 
 const recordAddtlController = {
 
-    getRecAddtl: async (req, res) => {
-        const foundIngredients = await Ingredient.find().sort({name: 1});
-        
+    getRecAddtl: async(req, res) => {
+        const foundIngredients = await Ingredient.find().sort({ name: 1 });
+
         await res.render('recordAddtlP1', {
-                ingredients: foundIngredients,
-            })
+            ingredients: foundIngredients,
+        })
     },
 
-    postRecAddtl1: async (req, res) => {
+    postRecAddtl1: async(req, res) => {
         // TODO: Search for INGREDIENT
         // TODO: Search for UNITS
-        const foundVariants = await IngreVariation.find({ingreID: req.body.ingreId});
+        const foundVariants = await IngreVariation.find({ ingreID: req.body.ingreId });
         const foundIngredient = await Ingredient.findById(req.body.ingreId);
         const foundUnits = await Unit.find();
 
@@ -29,55 +29,62 @@ const recordAddtlController = {
         })
     },
 
-    postRecAddtl2: async (req, res) => {
-        // For net weight accummulation
-        var totalNetWeight = 0;
+    postRecAddtl2: async(req, res) => {
+        try {
+            // For net weight accumulation
+            let totalNetWeight = 0;
 
-        // User Inputs
-        const ingredientId = req.body.ingreId
-        const variantId = req.body.ingreNetUnit;
-        const inputQty = req.body.ingreQty;
+            // User Inputs
+            const ingredientId = req.body.ingreId;
+            const variantId = req.body.ingreNetUnit;
+            const inputQty = req.body.ingreQty;
 
-        // Other variables
-        const foundIngredient = await Ingredient.findById(ingredientId);
-        var foundVariant;
+            // Other variables
+            const foundIngredient = await Ingredient.findById(ingredientId);
+            let foundVariant;
 
-        if(variantId == "others") {
-            const inputNetWt = req.body.ingreNetWt;
-            const inputUnit = req.body.ingreUnit;
-            const foundUnit = await Unit.findOne({unitSymbol: inputUnit});
+            if (variantId === "others") {
+                const inputNetWt = req.body.ingreNetWt;
+                const inputUnit = req.body.ingreUnit;
+                const foundUnit = await Unit.findOne({ unitSymbol: inputUnit });
 
-            const newIngreVariation = new IngreVariation({
-                ingreID: ingredientId,
-                unitID: foundUnit._id,
-                netWeight: inputNetWt,
-            }); 
+                const newIngreVariation = new IngreVariation({
+                    ingreID: ingredientId,
+                    unitID: foundUnit._id,
+                    netWeight: inputNetWt,
+                });
 
-            await newIngreVariation.save();
+                await newIngreVariation.save();
+                foundVariant = newIngreVariation;
+            } else {
+                foundVariant = await IngreVariation.findById(variantId);
+            }
 
-            foundVariant = await IngreVariation.findById(newIngreVariation._id);
+            const foundUnit = await Unit.findById(foundVariant.unitID);
 
-            // console.log("test: " + foundVariant._id)
-        } else {
-            foundVariant = await IngreVariation.findById(variantId);
-        }  
+            if (!foundIngredient.unitID.equals(foundUnit._id)) {
+                // Use conversion factor
+                const conFactor = await Conversion.findOne({
+                    initialUnitId: foundUnit._id,
+                    convertedUnitId: foundIngredient.unitID,
+                });
 
-        const foundUnit = await Unit.findById(foundVariant.unitID);
+                totalNetWeight = inputQty * (foundVariant.netWeight * conFactor.conversionFactor);
+                console.log("Conversion Factor:", conFactor.conversionFactor);
+            } else {
+                totalNetWeight = inputQty * foundVariant.netWeight;
+                console.log(totalNetWeight + "=" + inputQty + "*" + foundVariant.netWeight);
+            }
 
-        if(!foundIngredient.unitID.equals(foundUnit._id)){
-            // Use conversion factor
-            const conFactor = await Conversion.findOne({initialUnitId: foundUnit._id, convertedUnitId: foundIngredient.unitID});
-            // console.log("Confactor id: " + conFactor._id)
-            
-            totalNetWeight = inputQty * (foundVariant.netWeight * conFactor.conversionFactor);
-        }else{
-            totalNetWeight = inputQty * foundVariant.netWeight;
+            foundIngredient.totalNetWeight = Number(foundIngredient.totalNetWeight) + Number(totalNetWeight);
+            await foundIngredient.save();
+
+            res.status(500).send("Total net weight updated.");
+            // TODO: Success Page
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("An error occurred while updating the ingredient's running total.");
         }
-
-        // TODO:
-        //  [x] - Compute total net weight
-        //  [ ] - update Ingredient Object's running total
-        //  [ ] - Success Page
     }
 }
 
