923f9fa8b011407b78274fcf57419310b9c507f2
soP0ggers <133246977+ca-r-b@users.noreply.github.com>
2023-07-08 21:04:08 +0800

    Updated FRONTEND

--- controller/inputPhysicalController.js
+++ controller/inputPhysicalController.js
@@ -1,9 +1,34 @@
 const User = require('../model/usersSchema.js');
+const Ingredient = require("../model/ingredientsSchema.js");
+const Unit = require("../model/unitsSchema.js");
+const IngreVariation = require("../model/ingreVariationsSchema.js");
+const Conversion = require("../model/conversionSchema.js");
 const bcrypt = require("bcrypt");
 
 const inputPhysicalController = {
-    getInputPhysCount: function(req, res) {
-        res.render('inputPhysicalCount');
+    getInputPhysCount: async (req, res) => {
+        try {
+            // Retrieve all ingredients from the database
+            const ingredients = await Ingredient.find().sort({name: 1});
+
+            // Retrieve all units from the database
+            const units = await Unit.find();
+
+            // Map the unit IDs to unit symbols in the ingredients array
+            const ingredientsWithUnitSymbols = ingredients.map(ingredient => {
+                const unit = units.find(unit => unit._id.equals(ingredient.unitID));
+                return {
+                    ...ingredient.toObject(),
+                    unitSymbol: unit ? unit.unitSymbol : ''
+                };
+            });
+
+            // Pass the ingredients data to the view
+            res.render('inputPhysicalCount', {ingredients: ingredientsWithUnitSymbols});
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("An error occurred while retrieving the ingredients.");
+        }
     }
 }
 
