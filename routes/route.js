26150b1b3d88debdfd8f319bfded44278acbae25
soP0ggers <133246977+ca-r-b@users.noreply.github.com>
2023-07-17 00:41:33 +0800

    Added route + form

--- routes/route.js
+++ routes/route.js
@@ -99,6 +99,7 @@ app.get("/detailedReport/Milk", viewReportController.getDetailed)
 // Input Physical Count
 app.get("/inputPhysicalCount/p1", inputPhysicalController.getInputPhysCount)
 app.post("/inputPhysicalCount/p2", inputPhysicalController.postInputPhysCount1)
+app.post("/inputPhysicalCount/p2/process", inputPhysicalController.postInputPhysCount2)
 
 // Order Terminal
 app.get("/viewOrderTerminal", orderController.getOrder)
