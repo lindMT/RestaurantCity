3ff536b7c08812025d454eeace7728170eca9f7e
djdcaballero <don_caballero@dlsu.edu.ph>
2023-07-19 00:45:37 +0800

    Revised addConversion

--- controller/addConversionController.js
+++ controller/addConversionController.js
@@ -2,73 +2,54 @@ const { default: mongoose } = require('mongoose');
 const User = require('../model/usersSchema.js');
 const Unit = require('../model/unitsSchema.js');
 const Conversion = require('../model/conversionSchema.js');
+const Ingredients = require('../model/ingredientsSchema.js')
 const bcrypt = require("bcrypt");
 
 const addConversionController = {
     getAddConversion: async(req, res) => {
         const foundUnits = await Unit.find();
+        const foundIngredients = await Ingredients.find();
 
+        //TODO: Map units with ingredients
         await res.render('addConversion', {
-            units: foundUnits
+            units: foundUnits, ingredients: foundIngredients
         });
     },
     
     postAddConversion: async(req, res) => {
-        const fromUnitSymbol = req.body;
+        const inputIngredient = req.body;
         const toUnitSymbol = req.body;
         const inputFactor = req.body;
 
-        const fromUnit = await Unit.findOne({ unitSymbol: fromUnitSymbol}); // Subject to change if symbol or name will be used
-        const toUnit = await Unit.findOne({ unitSymbol: toUnitSymbol}); // Subject to change if symbol or name will be used
+        const toUnit = await Unit.findOne({ unitSymbol: toUnitSymbol }); // Subject to change if symbol or name will be used
+        const ingredient = await Ingredients.findOne({ name: inputIngredient });
 
         // Checks if conversion exists
         const existsConversion = await Conversion.findOne({
-            initialUnitId: fromUnit._id,
+            ingredientId: ingredient._id,
             convertedUnitId: toUnit._id
         });
 
         if(existsConversion){
             // Handle error where the conversion already exists
 
-        } else if(fromUnit.category !== toUnit.category && fromUnit.category !== 'Both' && toUnit.category !== 'Both'){
-            // Handle error where units do not belong in the same category or neither of them are "Both"
-
         } else{
             // Saves the conversion inputted by the user
+            const newSubUnit = [
+                {
+                    convertedUnitId: toUnit._id,
+                    conversionFactor: inputFactor
+                }
+            ]
             const newConversion = new Conversion({
-                initialUnitId: fromUnit._id,
-                convertedUnitId: toUnit._id,
-                conversionFactor: inputFactor
+                ingredientId: ingredient._id,
+                initialUnitId: ingredient.unitID,
+                subUnit: newSubUnit
             });
             
-            // Creates the reverse conversion inputted by the user
-            const reverseNewConversion = new Conversion({
-                initialUnitId: toUnit._id,
-                convertedUnitId: fromUnit._id,
-                conversionFactor: 1 / inputFactor
-            });
-
             await newConversion.save();
-            await reverseNewConversion.save();
         }
         
-        // Checks all existing conversions where the "toUnit" is the "fromUnit"
-        const findConversion = await Conversion.find({
-            initialUnitId: toUnit._id
-        });
-
-        for (const foundConversion of findConversion) {
-            if((foundConversion.convertedUnitId.toString() !== fromUnit._id.toString()) &&
-            ((fromUnit.category === foundConversion.category) || (foundConversion.category === "Both") || (fromUnit.category === "Both"))){ // Checks if both units have the same category OR the convertedUnit is "Both"
-                const newConversion = new Conversion({
-                    initialUnitId: fromUnit._id,
-                    convertedUnitId: foundConversion.convertedUnitId,
-                    conversionFactor: foundConversion.conversionFactor * inputFactor
-                });
-
-                await newConversion.save();
-            }
-        }
         return res.redirect('/addConversion');
     }
 
