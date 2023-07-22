491daccf71ef3cde1ea8bf9f368e7730554ae06d
elliamaeang <elliamae_ang@dlsu.edu.ph>
2023-07-23 01:12:59 +0800

    Stock Report: Partial Working Lost Column

--- controller/viewReportController.js
+++ controller/viewReportController.js
@@ -93,8 +93,13 @@ const viewReportController = {
             var dateArray = getDates(startDateObject, endDateObject);
             console.log(dateArray);
 
+            // purchased values
             var totalPurchased = 0;
             var purchasesValues = [];
+
+            // lost values
+            var totalLost = 0;
+            var lostValues = [];
 
             // loop through all ingredients
             for(var i = 0; i < ingres.length; i++){
@@ -102,10 +107,12 @@ const viewReportController = {
                 console.log("Current Ingre: " + ingres[i].name);
                 // loop through all dates
                 for (var d = 0; d < dateArray.length; d++){
+                    var date;
+
                     // loop through all purchases
                     for (var j = 0; j < purchases.length; j++){
                         // get date and convert string to Date type
-                        var date = new Date(purchases[j].date);
+                        date = new Date(purchases[j].date);
                         console.log(date);
                         // check if purchase date matches current date loop
                         if (date.getDate() === dateArray[d].getDate()){
@@ -153,9 +160,11 @@ const viewReportController = {
                                                 }
                                                 // convert netWeight
                                                 convertedVal = +(ingreVars[k].netWeight*multiplier);
+                                                console.log(convertedVal);
 
                                                 // add to totalPurchased
                                                 totalPurchased += +(convertedVal*purchases[j].qty);
+                                                console.log(totalPurchased);
                                             }
                                         }
                                     }
@@ -205,17 +214,124 @@ const viewReportController = {
                             }
                         }
                     }
+
+                    // loop through all discarded
+                    for (var a = 0; a < discardeds.length; a++){
+                        // get date and convert string to Date type
+                        date = new Date(discardeds[a].date);
+                        console.log("DISCARDEDS " + date);
+                        // check if discardeds date matches current date loop
+                        if (date.getDate() === dateArray[d].getDate()){
+                            // check if the discard is for the ingredient
+                            if (ingres[i]._id.toString() == discardeds[a].ingreID.toString()){
+                                console.log(discardeds[a]);
+                                // check if the discard is for a variant
+                                if (discardeds[a].varID !== undefined){
+                                    // with variant
+                                    console.log("WITH VARIANT");
+
+                                    // get variant
+                                    for (var k = 0; k < ingreVars.length; k++){
+                                        if (discardeds[a].varID.toString() == ingreVars[k]._id.toString()) {
+                                            // check if unit matches
+                                            if(ingres[i].unitID.toString() == ingreVars[k].unitID.toString()){
+                                                // if yes, add value as is
+                                                console.log("Unit Match For Has Variant");
+                                                console.log("NetWeight: " + ingreVars[k].netWeight);
+                                                console.log("Qty: " + discardeds[a].qty);
+                                                totalLost += +(ingreVars[k].netWeight*discardeds[a].qty);
+                                            }else{
+                                                //if no, convert
+                                                console.log("Unit Not Match For Has Variant");
+                                                var fromID = ingreVars[k].unitID.toString();
+                                                var toID = ingres[i].unitID.toString();
+                                                var multiplier = 0;
+                                                var convertedVal = 0;
+                                                // console.log(fromID);
+                                                // console.log(toID);
+                                                for (var l = 0; l < conversions.length; l++){
+                                                    // get conversion factor
+                                                    // console.log("InitUnitID: " + conversions[l].initialUnitId.toString());
+                                                    if (fromID == conversions[l].initialUnitId.toString()){
+                                                        // console.log("FromID Match Found");
+                                                        // console.log("ConvertedUnitID: " + conversions[l].convertedUnitId.toString());
+                                                        if (toID == conversions[l].convertedUnitId.toString()){
+                                                            // console.log("ToID Match Found");
+                                                            multiplier = conversions[l].conversionFactor;
+                                                            console.log("Multiplier: " + multiplier);
+                                                        }
+                                                    }
+                                                }
+                                                // convert netWeight
+                                                convertedVal = +(ingreVars[k].netWeight*multiplier);
+                                                console.log(convertedVal);
+
+                                                // add to totalPurchased
+                                                totalLost += +(convertedVal*discardeds[a].qty);
+                                                console.log(totalLost);
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    // no variant
+                                    console.log("NO VARIANT");
+                                    // check if unit matches
+                                    if(discardeds[a].unitID.toString() == ingres[i].unitID.toString()){
+                                        // if yes, add value as is
+                                        console.log("Unit Match For Has No Variant");
+                                        console.log("NetWeight: " + discardeds[a].netWeight);
+                                        totalLost += +discardeds[a].netWeight;
+                                    }else{
+                                        // if no, convert
+                                        console.log("Unit Not Match For Has No Variant");
+                                        var fromID = discardeds[a].unitID.toString();
+                                        var toID = ingres[i].unitID.toString();
+                                        var multiplier = 0;
+                                        var convertedVal = 0;
+                                        // console.log("FromID: " + fromID);
+                                        // console.log("ToID: " + toID);
+                                        for (var l = 0; l < conversions.length; l++){
+                                            // get conversion factor
+                                            // console.log("InitUnitID: " + conversions[l].initialUnitId.toString());
+                                            if (fromID == conversions[l].initialUnitId.toString()){
+                                                // console.log("FromID Match Found");
+                                                // console.log("ConvertedUnitID: " + conversions[l].convertedUnitId.toString());
+                                                if (toID == conversions[l].convertedUnitId.toString()){
+                                                    // console.log("ToID Match Found");
+                                                    multiplier = conversions[l].conversionFactor;
+                                                    console.log("Multiplier: " + multiplier);
+                                                }
+                                            }
+                                        } 
+                                        // convert netWeight
+                                        convertedVal = discardeds[a].netWeight*multiplier;
+                                        console.log("Converted: " + convertedVal);
+
+                                        // add to totalPurchased
+                                        // console.log("totalLost: "+ totalLost);
+                                        totalLost += +convertedVal;
+                                        console.log("totalLost: "+ totalLost);
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
                 purchasesValues[i] = totalPurchased;
                 console.log("Purchased: " + totalPurchased);
                 console.log("purchasesValues[" + i + "] = " + purchasesValues[i]);
                 totalPurchased = 0;
+
+                lostValues[i] = totalLost;
+                console.log("Lost: " + totalLost);
+                console.log("lostValues[" + i + "] = " + lostValues[i]);
+                totalLost = 0;
             }
             
             // console.log("ingres.length = " + ingres.length);
             // console.log("purchasesValues length = " + purchasesValues.length);
             // console.log(ingres);
-            res.render('postCustom', {ingres, ingreVars, units, conversions, dishes, dishRecipes, orders, orderItems, purchases, discardeds, mismatches, purchasesValues, startDate, endDate, formattedStartDate, formattedEndDate});
+            res.render('postCustom', {ingres, ingreVars, units, conversions, dishes, dishRecipes, orders, orderItems, purchases, discardeds, mismatches, purchasesValues, lostValues, startDate, endDate, formattedStartDate, formattedEndDate});
         } catch (error) {
             console.error(error);
             res.status(500).send("An error occurred");
