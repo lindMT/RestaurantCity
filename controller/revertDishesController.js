029d59876a9ce71ea4fe42c7710282a158bc001f
djdcaballero <don_caballero@dlsu.edu.ph>
2023-07-28 00:57:05 +0800

    Revert dishes revisions

--- controller/revertDishesController.js
+++ controller/revertDishesController.js
@@ -14,30 +14,21 @@ const revertDishesController = {
             // Retrieve all dishes that are for approval
             
               // Retrieve approved dish recipes that have 'for approval' status
-            const approvedDishRecipes = await DishRecipe.find({
+              const approvedDishRecipes = await DishRecipe.find({
                 isActive: false,
                 isApproved: 'approved'
-            });
-
-            const recipeMap = new Map();
-            approvedDishRecipes.forEach((recipe) => {
-                const dishID = recipe.dishID.toString();
-                if (!recipeMap.has(dishID)) {
-                    recipeMap.set(dishID, []);
-                }
-                recipeMap.get(dishID).push(recipe);
-            });
-
-            const approvedDishIds = approvedDishRecipes.map((recipe) => recipe.dishID);
+              });
               
-            // Find the corresponding approved dishes using the dishID field from approvedDishRecipes
-            const dishes = await Dish.find({
+              const approvedDishIds = approvedDishRecipes.map(recipe => recipe.dishID);
+              
+              // Find the corresponding approved dishes using the dishID field from approvedDishRecipes
+              const dishes = await Dish.find({
                 $or: [
                     { _id: { $in: approvedDishIds }, isActive: true },
                     { _id: { $in: approvedDishIds }, isActive: false }
                 ],
                 isApproved: 'approved'
-            });
+              });
 
             
             // Retrieve categories from DishCatego
@@ -48,7 +39,8 @@ const revertDishesController = {
                 const category = categories.find(category => category._id.equals(dish.categoryID));
                 
                 // Fetch the recipe for the dish
-                const recipes = recipeMap.get(dish._id.toString()) || [];
+                const recipes = await DishRecipe.find({ dishID: dish._id, isActive: false }).lean();
+
 
                 // Map ingredient names to the recipe items
                 const recipeWithIngredients = await Promise.all(recipes.map(async recipe => {
