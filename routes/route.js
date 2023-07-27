fe5f0c1535bf2ddcb5d34202d1ea1c3b7e82d68a
JamesLorencee <james.seril.12@gmail.com>
2023-07-27 21:48:03 +0800

    Added revertDishes Module
    TODO:
    Backend

--- routes/route.js
+++ routes/route.js
@@ -10,6 +10,7 @@ const recordFirstController = require('../controller/recordFirstController.js')
 const recordAddtlController = require('../controller/recordAddtlController.js')
 const manageDishesController = require('../controller/manageDishesController.js');
 const approveDishesController = require('../controller/approveDishesController.js');
+const revertDishesController = require('../controller/revertDishesController.js');
 const editDishController = require('../controller/editDishController.js');
 const viewReportController = require('../controller/viewReportController.js');
 const viewOrderReportController = require("../controller/viewOrderReportController.js");
@@ -72,6 +73,10 @@ app.post("/manageDishes/process", manageDishesController.postManageDishes);
 
 app.get("/approveDishes", approveDishesController.getApproveDishes);
 app.post("/approveDish/process", approveDishesController.postApproveDish)
+
+// Revert Dishes
+app.get("/revertDishes", revertDishesController.getRevertDishes);
+// app.post("/revertDish/process", revertDishesController.postRevertDishes)
 
 // Add Dish
 app.get("/addDish", addDishController.getAddDish)
