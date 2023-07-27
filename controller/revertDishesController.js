56b339d3f8c6aa89646c246b3e07c7a0561e44e4
djdcaballero <don_caballero@dlsu.edu.ph>
2023-07-27 23:20:31 +0800

    Revised revert dish

--- controller/revertDishesController.js
+++ controller/revertDishesController.js
@@ -8,9 +8,27 @@ const revertDishesController = {
 
     getRevertDishes: async function(req, res) {
         try {
-            // Retrieve all dishes available
-            const dishes = await Dish.find({ isActive: false, isApproved: 'approved' }).collation({ locale: 'en' }).sort({ name: 1 });
-           
+            // Retrieve all dishes that are for approval
+            const dishesForApproval = await Dish.find({
+                isActive: false,
+                isApproved: 'approved'
+              });
+          
+              // Retrieve approved dish recipes that have 'for approval' status
+              const approvedDishRecipes = await DishRecipe.find({
+                isActive: false,
+                isApproved: 'approved'
+              });
+          
+              // Find the corresponding approved dishes using the dishID field from approvedDishRecipes
+              const approvedDishes = await Dish.find({
+                _id: { $in: approvedDishRecipes.map(recipe => recipe.dishID) },
+                isActive: false,
+                isApproved: 'approved'
+              });
+
+              const dishes = [...dishesForApproval, ...approvedDishes];
+            
             // Retrieve categories from DishCatego
             const categories = await DishCategory.find();
             
@@ -19,37 +37,35 @@ const revertDishesController = {
                 const category = categories.find(category => category._id.equals(dish.categoryID));
                 
                 // Fetch the recipe for the dish
-                const recipes = await DishRecipe.find({ dishID: dish._id, isActive: false, isApproved: 'approved' }).lean();
+                const recipe = await DishRecipe.findOne({ dishID: dish._id }).lean();
+
+                // Retrieve ingredient names for the recipe
+                const ingredientIds = recipe ? recipe.ingredients.map(item => item.ingredient) : [];
+                const ingredients = await Ingredients.find({ _id: { $in: ingredientIds } }, 'name').lean();
 
                 // Map ingredient names to the recipe items
-                const recipesWithIngredients = await Promise.all(recipes.map(async recipe => {
+                const recipeWithIngredientNames = recipe ? await Promise.all(recipe.ingredients.map(async item => {
+                    const ingredient = ingredients.find(ingredient => ingredient._id.equals(item.ingredient));
 
-                    const ingredientIds = recipe.ingredients.map(item => item.ingredient);
-                    const ingredients = await Ingredients.find({ _id: { $in: ingredientIds } }, 'name').lean();
+                    // Fetch the chefUnit with unitSymbol
+                    const chefUnit = await Units.findOne({ _id: item.chefUnitID }, 'unitSymbol').lean();
 
-                    const recipeWithIngredientNames = await Promise.all(recipe.ingredients.map(async item => {
-                        const ingredient = ingredients.find(ingredient => ingredient._id.equals(item.ingredient));
-                        const chefUnit = await Units.findOne({ _id: item.chefUnitID }, 'unitSymbol').lean();
-                        return {
-                            ...item,
-                            ingredientName: ingredient ? ingredient.name : '',
-                            chefUnitSymbol: chefUnit ? chefUnit.unitSymbol : ''
-                        };
-                    }));
                     return {
-                        ...recipe,
-                        ingredients: recipeWithIngredientNames,
+                        ...item,
+                        ingredientName: ingredient ? ingredient.name : '',
+                        chefUnitSymbol: chefUnit ? chefUnit.unitSymbol : ''
                     };
-                }));
+                })) : [];
+
                 return {
                     ...dish.toObject(),
                     category: category ? category.category : '',
-                    recipe: recipesWithIngredients
+                    recipe: recipeWithIngredientNames
                 };
             }));
-            console.log(dishesWithCategory)
+
             //Pass dishes to views
-            res.render('revertDishes', { dishes: dishesWithCategory });
+            res.render('approveDishes', { dishes: dishesWithCategory });
         } catch (error) {
             console.error(error);
             res.status(500).send("An error occurred while retrieving the dishes.");
